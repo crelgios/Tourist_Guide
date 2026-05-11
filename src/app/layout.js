6e5797eded23e1c48be0aa85235acf248f0a1396
crelgios <creligios@gmail.com>
2026-05-11 09:14:45 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://www.aliwvide.com";
 
@@ -48,7 +49,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-2QF326WF22"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-2QF326WF22');
+          `}
+        </Script>
+
+        {children}
+      </body>
     </html>
   );
 }
